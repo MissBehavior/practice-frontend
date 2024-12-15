7038433d7379db6c0eac91227e847379e1072eaf
MissBehavior <viktorija.jakubcevic@gmail.com>
2024-12-15 12:09:12 +0100

    random posts added to news

--- src/pages/posts-external/post-detail.tsx
+++ src/pages/posts-external/post-detail.tsx
@@ -17,10 +17,15 @@ import { useTranslation } from 'react-i18next'
 import Breadcrumb from '@/components/breadcrumb'
 import Tag from './tag'
 import i18n from '@/i18n/config'
+interface PostDetailType {
+    post: PostData
+    randomPosts: PostData[]
+}
 
 const PostDetail: React.FC = () => {
     const { id } = useParams<{ id: string }>()
     const [post, setPost] = useState<PostData | null>(null)
+    const [randomPosts, setRandomPosts] = useState<PostData[] | null>([])
     const [loading, setLoading] = useState<boolean>(true)
     const [error, setError] = useState<string | null>(null)
     const navigate = useNavigate()
@@ -34,12 +39,13 @@ const PostDetail: React.FC = () => {
 
         const fetchPost = async () => {
             try {
-                const response = await axios.get<PostData>(
+                const response = await axios.get<PostDetailType>(
                     `http://localhost:3000/posts/${id}`
                 )
                 console.log('lllllllllllllllllllllll')
                 console.log(response.data)
-                setPost(response.data)
+                setPost(response.data.post)
+                setRandomPosts(response.data.randomPosts)
             } catch (err: any) {
                 console.error(err)
                 if (err.response) {
@@ -183,6 +189,68 @@ const PostDetail: React.FC = () => {
                                     : post.contentLT}
                             </div>
                         </div>
+                        <aside
+                            aria-label="Related Articles"
+                            className="mx-auto mt-10 max-w-screen-xl py-20"
+                        >
+                            <h2 className="mb-8 text-center text-5xl font-bold text-gray-900">
+                                {t('morenews')}
+                            </h2>
+
+                            <div className="mx-auto grid max-w-screen-lg justify-center px-4 sm:grid-cols-2 sm:gap-6 sm:px-8 md:grid-cols-3">
+                                {randomPosts &&
+                                    randomPosts.map((randomPost) => (
+                                        <article className="mx-auto my-4 flex flex-col overflow-hidden rounded-lg border border-gray-300 bg-white text-gray-900 transition hover:translate-y-2 hover:shadow-lg">
+                                            <Link
+                                                to={`/post-external/${randomPost._id}`}
+                                            >
+                                                <img
+                                                    src={randomPost.postPicture}
+                                                    className="h-56 w-full object-cover"
+                                                    alt=""
+                                                />
+                                                <div className="flex-auto px-6 py-5">
+                                                    <span className="mb-2 flex items-center text-sm font-semibold">
+                                                        <svg
+                                                            xmlns="http://www.w3.org/2000/svg"
+                                                            className="mr-2 h-4 w-4"
+                                                            fill="none"
+                                                            viewBox="0 0 24 24"
+                                                            stroke="currentColor"
+                                                            stroke-width="2"
+                                                        >
+                                                            <path
+                                                                stroke-linecap="round"
+                                                                stroke-linejoin="round"
+                                                                d="M7 20l4-16m2 16l4-16M6 9h14M4 15h14"
+                                                            />
+                                                        </svg>
+                                                        {format(
+                                                            new Date(
+                                                                randomPost.createdAt
+                                                            ),
+                                                            'MMMM d, yyyy'
+                                                        )}
+                                                    </span>
+                                                    <h3 className="mt-4 mb-3 text-xl font-semibold xl:text-2xl">
+                                                        {randomPost.title}
+                                                    </h3>
+                                                    <p className="mb-4 text-base font-light">
+                                                        {randomPost.content.substring(
+                                                            0,
+                                                            50
+                                                        )}
+                                                        ...
+                                                    </p>
+                                                    <span className="inline-block cursor-pointer select-none rounded-full border border-gray-800 bg-gray-800 px-2 py-1 text-center align-middle text-sm font-semibold leading-normal text-white no-underline shadow-sm">
+                                                        Read Now
+                                                    </span>
+                                                </div>
+                                            </Link>
+                                        </article>
+                                    ))}
+                            </div>
+                        </aside>
                     </div>
                 </div>
             </div>
